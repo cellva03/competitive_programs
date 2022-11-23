4170bec427296558d57d6807d3d7141df628b977
cellva03 <selvavignesh432@gmail.com>
2022-11-23 13:52:20 +0530

    PRIMEDICE

--- PRIMEDICE.py
+++ PRIMEDICE.py
@@ -2,10 +2,12 @@ t = int(input())
 while t:
     a, b = map(int,input().split())
     flag = True
-    for i in range(2,(a+b)):
-        if (a+b)%i == 0:
-            flag = False
-            break
+    if (a+b)>2:
+        for i in range(2,(a+b)):
+            if (a+b)%i == 0:
+                flag = False
+                break
     if flag:
-        print("")
+        print("Alice")
+    
         
