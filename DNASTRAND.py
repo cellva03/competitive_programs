c5e8a3f4511f4aada2964cc05c5de6fce98f0309
cellva03 <selvavignesh432@gmail.com>
2022-11-24 14:21:58 +0530

    DNASTRAND

--- DNASTRAND.py
+++ DNASTRAND.py
@@ -2,4 +2,15 @@ t = int(input())
 while t:
     n = int(input())
     s = str(input())
-    
+    res = ''
+    for i in s:
+        if i == 'A':
+            res+='T'
+        elif i == 'T':
+            res+='A'
+        elif i == 'G':
+            res+='C'
+        elif i == 'C':
+            res+= 'G'
+    print(res)
+    t-=1
