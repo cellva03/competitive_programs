76f3eeb8fd5b0f67ab49b6a4ba8789cca2e09814
cellva03 <selvavignesh432@gmail.com>
2022-11-26 11:39:49 +0530

    wights

--- WGHTS.py
+++ WGHTS.py
@@ -2,7 +2,7 @@
 t = int(input())
 while t:
     w, x, y, z = map(int,input().split())
-    if (x+y) == w or (x+z) == w or (y+z) == w:
+    if (x+y) == w or (x+z) == w or (y+z) == w or x == w or y == w or z == w or (x+y+z) == w:
         print("yes")
     else:
         print("no")
