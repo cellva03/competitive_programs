9fd2ebb115f2532104c66e6a808274ee47cedceb
cellva03 <selvavignesh432@gmail.com>
2022-11-27 13:49:12 +0530

    CHEFAPPS

--- CHEFAPPS.py
+++ CHEFAPPS.py
@@ -1,4 +1,12 @@
 t = int(input())
 while t:
     s, x, y, z = map(int,input().split())
-    
+    tot = x+y+z
+    if s >= tot:
+        print(0)
+    else:
+        if (tot-max(x,y)) <= s:
+            print(1)
+        else:
+            print(2)
+    t
