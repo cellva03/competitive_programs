0cc8d2a43a170ac3c2f23334898854c39d1ae977
cellva03 <selvavignesh432@gmail.com>
2022-12-02 19:51:23 +0530

    PASSTHEEXAM

--- PASSTHEEXAM.py
+++ PASSTHEEXAM.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
     a, b, c = map(int,input().split())
-    print("yes") if a>10 and b>10 and c>10
+    print("yes") if a>10 and b>10 and c>10 and (a+b+c)>=100 else print("no")
