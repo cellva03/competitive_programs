07554d8a49146edb588741d9dcd6ee3c6fc4b8a1
cellva03 <selvavignesh432@gmail.com>
2022-12-09 14:03:09 +0530

    water

--- WATERFLOW.py
+++ WATERFLOW.py
@@ -2,4 +2,7 @@ for _ in range(int(input())):
     w, x, y, z = map(int,input().split())
     if ((y*z)+x) > x :
         print("overflow")
-    elif ((y*z)+x) > x
+    elif ((y*z)+x) < x:
+        print("unfilled")
+    else:
+        
