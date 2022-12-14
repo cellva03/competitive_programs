1bf064549c6487819dd75efb2fefa78111ad129f
cellva03 <selvavignesh432@gmail.com>
2022-12-14 13:55:35 +0530

    cgym

--- CGYM.py
+++ CGYM.py
@@ -1,8 +1,8 @@
 for _ in range(int(input())):
     x, y, z = map(int,input().split())
-    if x > z:
+    if x >= z:
         print(0)
-    elif (x+y) < z:
+    elif (x+y) <= z:
         print(2)
     else:
         print(1)
