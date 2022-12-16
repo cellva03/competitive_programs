84ade8dd6fb9fb81461b6941b4416c40284589a6
cellva03 <selvavignesh432@gmail.com>
2022-12-16 20:24:52 +0530

    avg

--- AVGPROBLEM.py
+++ AVGPROBLEM.py
@@ -1,6 +1,7 @@
+# cook your dish here
 for _ in range(int(input())):
     x, y, z = map(int,input().split())
-    res = float(x+y)
+    res = float(x+y)//2
     if res > z:
         print("Yes")
     else:
