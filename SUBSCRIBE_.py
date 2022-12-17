5f9ace837da4e184e7a8f907cb45c2d2a861c79d
cellva03 <selvavignesh432@gmail.com>
2022-12-17 17:20:28 +0530

    sub

--- SUBSCRIBE_.py
+++ SUBSCRIBE_.py
@@ -1,7 +1,10 @@
 for _ in range(int(input())):
     n, x = map(int,input().split())
     if n > 6:
-        res = (n//6)+1
+        if n%6==0:
+            res = (n//6)
+        else:
+            res = (n//6)+1
         print(res*x)
     else:
         print(x)
