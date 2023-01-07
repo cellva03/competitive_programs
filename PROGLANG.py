d66d01d60b8c1c770344bfc2a0b912cfb2afbada
cellva03 <selvavignesh432@gmail.com>
2023-01-07 14:15:56 +0530

    prog

--- PROGLANG.py
+++ PROGLANG.py
@@ -2,3 +2,4 @@ for _ in range(int(input())):
     a, b, a1, b1, a2, b2 = list(map(int,input().split()))
     if((a == a1 and b == b1) or (a == b1 and b == a1)):
         print(1)
+    elif((a == a2 and b == b2) or (a == b2 and b == a2)):
