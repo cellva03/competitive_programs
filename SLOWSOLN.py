27040c87ede12cb4058cc72000111ed4aaafe927
Selva <88723923+cellva03@users.noreply.github.com>
2023-01-13 10:45:22 +0530

    Update SLOWSOLN.py

--- SLOWSOLN.py
+++ SLOWSOLN.py
@@ -3,3 +3,9 @@ t=int(input())
 for _ in range(t):
     x,y,z=map(int,input().split())
     c=x*y
+    if(c>=z):
+      a=z//y
+      k=y*a 
+      g=z-k
+      result=(a*y*y)+g*g
+      print(result)
