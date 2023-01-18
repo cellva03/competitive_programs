1fc345217f92379b6e56359e3821c28c7b7c116d
cellva03 <selvavignesh432@gmail.com>
2023-01-18 11:04:54 +0530

    seq bin

--- seq_bin.py
+++ seq_bin.py
@@ -3,4 +3,8 @@ for _ in range(int(input())):
     if n == 3:
         print('010')
     elif n >= 4:
-        
+        for i in range(0,n):
+            if i == 0:
+                print('0',end='')
+            elif i == 1:
+                pr
